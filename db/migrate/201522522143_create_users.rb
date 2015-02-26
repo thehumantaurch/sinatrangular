9970aaf5270f97440fbbbb23732555b84bea34e3
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-02-25 22:49:28 -0500

    edit user model/migration

--- db/migrate/201522522143_create_users.rb
+++ db/migrate/201522522143_create_users.rb
@@ -1,13 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      
-      t.string :email
-      
-      t.string :password
-      
-      t.integer :access_level
-      
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.integer :access_level, default: 1
+      t.timestamps
     end
   end
 end
--- models/users.rb
+++ models/users.rb
@@ -1,12 +1,5 @@
 class Users < ActiveRecord::Base
-  
+  has_secure_password
   validates :email, presence: true
-  
-  
-  validates :password, presence: true
-  
-  
   validates :access_level, presence: true
-  
-  
 end
